3ef53f1f4432c0579deb7925a4c8adcb61d7382a
Sweeper01 <sweeper1986@gmx.de>
2022-05-08 11:21:20 +0200

    commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
+import 'typeface-roboto'
+import '@mdi/font/css/materialdesignicons.css'
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import vuetify from "./plugins/vuetify";
-
 Vue.config.productionTip = false;
 
 new Vue({
