06c02e21894f03f7be70dd5a4e14b4cfbe34fb64
Sweeper01 <sweeper1986@gmx.de>
2024-03-22 15:30:37 +0100

    gn-tools

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? "/GN-Tools-Site/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/GN-Tools/" : "/",
 });
